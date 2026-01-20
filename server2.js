13abfce764475e3d431b2f551d0f920010f3c03b
Aakhya Sahu <aakhyasahu8@gmail.com>
2026-01-20 21:38:21 +0530

    backend files

---
+++ server2.js
@@ -0,0 +1,20 @@
+// objects
+
+var obj = {
+  a:"aakhya",
+  b:112,
+  c:"fruits"
+}
+console.log(obj.a);
+Object.freeze(obj); // this freeze the values of the object and cannot be change further
+
+function abcd(a,d,c){
+       
+}
+console.log(abcd.length);  // gives the length of the function
+
+function as(){
+  return 12;
+}
+var ans = as();
+console.log(ans);
