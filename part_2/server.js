05e1e0e7ca9e0b4ac3bc12721935bc391437ec54
Aakhya Sahu <aakhyasahu8@gmail.com>
2026-01-23 20:00:08 +0530

    Initial backend upload

--- part_2/server.js
+++ part_2/server.js
@@ -21,7 +21,31 @@ const fs = require('fs');
 //   else console.log("done");
 // })
 //appendFile
-fs.appendFile("hey.txt"," me toh aacha hu",function(err){
-  if(err) console.error(err);
-  else console.log("done");
-})
+// fs.appendFile("hey.txt"," me toh aacha hu",function(err){
+//   if(err) console.error(err);
+//   else console.log("done");
+// })
+
+//renameFile
+// fs.rename("hey.txt","hello.txt",function(err){
+//   if(err) console.error(err);
+//   else console.log("done");
+// })
+
+//copyfile
+// fs.copyFile("hello.txt","./copy/hey.txt",function(err){
+//   if(err) console.error(err);
+//   else console.log("done");
+// })
+
+// //unlike
+// fs.unlink("hello.txt",function(err){
+//   if(err) console.log(err);
+//   else console.log("removed");
+// })
+
+//mkdir
+fs.rm("./copy", { recursive: true, force: true }, function(err){
+    if(err) console.error(err);
+    else console.log("folder removed");
+});
