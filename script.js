4158023cb3627a257333fb2c118ba02f3befc2f7
Aakhya Sahu <aakhyasahu8@gmail.com>
2026-01-27 20:01:10 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// form handling and working with forms
+// handle backend process of forms and making sure the data coming from any frontend library,fw,templating engines,we still handle it at backend
+
+// cookie and session 
+//hum log khch bhi data frontend par browser per rkh skte hai and jab bhi aap khch bhi request backend per karoge wo frontend per saved data automatically backend per chla jaega
